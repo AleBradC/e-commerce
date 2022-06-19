6951216d36cc720a3bf9609285655de8c88b7826
AleBradC <alexandrabradclaudia@gmail.com>
2022-06-19 18:04:01 +0300

    Created product card

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,22 +1,16 @@
 import mongoose from 'mongoose'
 
-const productSchema = new mongoose.Schema(
-  {
-    id: Number,
-    brand: String,
-    name: String,
-    category: String,
-    tags: [],
-    imageURL: String,
-    hoverImageURL: String,
-    rating: Number,
-    price: Number,
-  },
-  {
-    value: String,
-    _id: false,
-  }
-)
+const productSchema = new mongoose.Schema({
+  id: Number,
+  brand: String,
+  name: String,
+  category: String,
+  tags: [],
+  imageURL: String,
+  hoverImageURL: String,
+  rating: Number,
+  price: Number,
+})
 
 const Product = mongoose.model('Product', productSchema)
 
